a193a8ed18b1a6e9f95bb136cd25ae95dad8977b
Aya <pixelmixery@gmail.com>
2022-07-31 22:25:50 +1000

    basic calculator functions added

--- server.js
+++ server.js
@@ -12,10 +12,48 @@ const addNumbers = (number1, number2) => {
   return result;
 };
 
+const subNumbers = (number1, number2) => {
+  var num1 = parseInt(number1);
+  var num2 = parseInt(number2);
+  var result = num1 - num2;
+  return result;
+};
+const mulNumbers = (number1, number2) => {
+  var num1 = parseInt(number1);
+  var num2 = parseInt(number2);
+  var result = num1 - num2;
+  return result;
+};
+const divNumbers = (number1, number2) => {
+  var num1 = parseInt(number1);
+  var num2 = parseInt(number2);
+  var result = num1 - num2;
+  return result;
+};
+
 app.get("/addTwoNumbers", (req, res) => {
   var number1 = req.query.number1;
   var number2 = req.query.number2;
   var result = addNumbers(number1, number2);
+  res.json({ statusCode: 200, data: result, message: "Success" });
+});
+
+app.get("/subTwoNumbers", (req, res) => {
+  var number1 = req.query.number1;
+  var number2 = req.query.number2;
+  var result = subNumbers(number1, number2);
+  res.json({ statusCode: 200, data: result, message: "Success" });
+});
+app.get("/mulTwoNumbers", (req, res) => {
+  var number1 = req.query.number1;
+  var number2 = req.query.number2;
+  var result = mulNumbers(number1, number2);
+  res.json({ statusCode: 200, data: result, message: "Success" });
+});
+app.get("/divTwoNumbers", (req, res) => {
+  var number1 = req.query.number1;
+  var number2 = req.query.number2;
+  var result = divNumbers(number1, number2);
   res.json({ statusCode: 200, data: result, message: "Success" });
 });
 
