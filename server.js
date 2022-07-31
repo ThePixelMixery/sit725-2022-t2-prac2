a6f92c5c8dae8483e621adada524ad2fbe5b91cd
Aya <pixelmixery@gmail.com>
2022-07-31 22:22:45 +1000

    server.js Fixed

--- server.js
+++ server.js
@@ -1,35 +1,25 @@
 var express = require("express");
 var app = express();
-var port = process.env.port || 3000;
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
+const addNumbers = (number1, number2) => {
+  var num1 = parseInt(number1);
+  var num2 = parseInt(number2);
+  var result = num1 + num2;
+  return result;
+};
 
+app.get("/addTwoNumbers", (req, res) => {
+  var number1 = req.query.number1;
+  var number2 = req.query.number2;
+  var result = addNumbers(number1, number2);
+  res.json({ statusCode: 200, data: result, message: "Success" });
+});
 
-const addNumbers = (num1, num2) => {​
-
-    var number1 = parseInt(num1)​
-    
-    var number2 = parseInt(num2)​
-    
-    var result = number1 + number2;​
-    
-    return result; ​
-    
-    }
-
-app.get("/addTwoNumbers", (req,res) => {​
-
-    var num1 = req.query.num1;​
-    var num2 = req.query.num2; ​
-    var result = addNumbers(num1, num2);​
-
-    Res.json({statusCode: 200, data: result, message:"Success"})​
-    
-    }) 
-
+var port = process.env.port || 3000;
 app.listen(port, () => {
-    console.log("App listening to: " + port);
-    });
+  console.log("App running at http://localhost:" + port);
+});
